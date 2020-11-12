8a8f83655de9a16c243e3b5b7cf95fa0cc7883d4
Murat Cetin <mcetin@gmail.com>
2020-11-12 10:46:03 -0800

    Initial commit

--- src/Day03GitHubClass/main.java
+++ src/Day03GitHubClass/main.java
@@ -1,8 +1,9 @@
-package Main;
+package Day03GitHubClass;
 
 public class main {
     public static void main(String[] args) {
         System.out.println("hello Git Last day");
+        System.out.println("B21 is slowly learning");
 
     }
 }
